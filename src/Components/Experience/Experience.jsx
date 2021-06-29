32b60809c299692bc5f33dc7a637b0f07c43d84c
PatrickVuscan <patrick.vuscan@gmail.com>
2021-06-28 21:35:19 -0400

    Added new experiences, and refactored Experience content

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -1,75 +1,46 @@
 import React from 'react';
 import '../../App.scss';
 import { Body, Title, SuperText } from '../General/General';
+import { experience, projects } from './content';
 
 const Experience = () => {
   const onResumeClick = () => {
     window.open(`${process.env.PUBLIC_URL}/resume.pdf`);
   };
 
+  const experienceNodes = [];
+
+  experience.forEach(exp => {
+    experienceNodes.push(
+      <>
+        <SuperText>{exp.company}</SuperText>
+        <SuperText className="text-super-sub">{exp.role}</SuperText>
+        <Body>{exp.description}</Body>
+      </>,
+    );
+  });
+
   return (
     <div className="container-list">
       <div className="container-for-centering">
         <div className="work-experience">
           <Title>Work Experience</Title>
-          <SuperText>{'Scotiabank\'s Digital Factory'}</SuperText>
-          <SuperText className="text-super-sub">
-            Software Developer
-          </SuperText>
-          <Body>
-            {'I was the only software developer intern brought into the Digital Factory ' +
-            'during the summer of 2020. I developed for the front-end of a web application ' +
-            'that manages Scotiabank’s web and native campaign and offer presentation, ' +
-            'using technologies such as ReactJS and Redux. Took ownership over regions of ' +
-            'the app. Wrote API endpoints in the NodeJS backend, wrote unit tests using ' +
-            'React’s Testing Library, and refactored code to modernize the application.\n\n' +
-            'Placed 1st out of more than 80 teams in the bank wide Intern Capstone project, ' +
-            'aimed to find new ways to increase customer acquisition across the bank. After ' +
-            'engaging with customers and managers across different business lines, I developed ' +
-            'a Figma prototype of an educational solution which could be built to work in ' +
-            'conjunction with existing Scotiabank technology and analytic tools'}
-          </Body>
-          <SuperText>catchUp</SuperText>
-          <SuperText className="text-super-sub">
-            Software Developer
-          </SuperText>
-          <Body>
-            {'Lead developer and designer in a start-up, catchUp, a note sharing service that ' +
-            'focuses on working with accessibility needs guidelines. Designed the application ' +
-            'and flows using Figma. Developing the Front-end using technologies such as ReactJS, ' +
-            'React Routing, to deliver a clean minimalist user experience.'}
-          </Body>
-          <SuperText>NexJ Health Inc.</SuperText>
-          <SuperText className="text-super-sub">
-            Software Developer
-          </SuperText>
-          <Body>
-            {'Developed a web and native Android and iOS applications for NexJ Health Inc’s ' +
-            'patient-facing healthcare platform as a full stack developer.\n\n' +
-            'Built new user functionality and features, while working on the application’s ' +
-            'front-end UI through a custom in-house framework in JavaScript, creating back-end ' +
-            'services with Node JS, and interacting with the MongoDB database under a Mongoose ' +
-            'schema. Worked on the deployment of the web application into native applications ' +
-            'through Adobe PhoneGap.'}
-          </Body>
+          {experienceNodes}
         </div>
       </div>
+
       <div className="container-for-centering" style={{ backgroundColor: '#2d8000' }}>
         <div className="project-experience">
           <Title>Projects</Title>
           <div>
-            <SuperText>Scotiabank MVP Web Application</SuperText>
+            <SuperText>{projects[0].name}</SuperText>
             <SuperText className="text-super-sub">
-              Technology Leaderhip Initiatve x Scotiabank Collaboration
+              {projects[0].collab}
             </SuperText>
           </div>
           <Body>
-            {'Developed an MVP for Scotiabank, a reactive web application that utilizes ' +
-            'Interac’s new Request to Pay feature for businesses.\n\nBuilt an application with ' +
-            'React JS, and created RESTful back-end services and APIs with Express and Node ' +
-            'JS. Created and set up servers to host the application on Heroku with a ' +
-            'PostgreSQL database.\n\n' +
-            'You can demo the project '}
+            {projects[0].description}
+            {'\n\nYou can demo the project '}
             <a href="https://request-to-pay.herokuapp.com/">here!</a>
             {'\nYou can also see the '}
             <a href="https://github.com/PatrickVuscan/RequestToPay_FrontEnd">front-end repo here</a>
@@ -78,16 +49,14 @@ const Experience = () => {
             {' as well!'}
           </Body>
           <div>
-            <SuperText>This Website!</SuperText>
+            <SuperText>{projects[1].name}</SuperText>
             <SuperText className="text-super-sub">
-              Stand alone project
+              {projects[1].collab}
             </SuperText>
           </div>
           <Body>
-            {'Created this website from the ground up, using technologies such as React, React ' +
-            'Router, all of the latest React Hooks, entirely custom CSS, and Firebase for the ' +
-            'back-end.\n\n'}
-            {'You can also see the '}
+            {projects[1].description}
+            {'\n\nYou can also see the '}
             <a href="https://github.com/PatrickVuscan/personal-website">repo here</a>
             {' too!'}
           </Body>
---
+++ src/Components/Experience/content.js
@@ -0,0 +1,49 @@
+/* eslint-disable max-len */
+
+const experience = [
+  {
+    company: 'Riot Games',
+    role: 'Software Developer',
+    description: 'Developing the League of Legends client, used by more than a hundred million monthly users!\n\nIdentified and fixed memory leaks across the League of Legends client in EmberJS and C++, reducing long session memory use.',
+  },
+  {
+    company: 'Presize.ai',
+    role: 'Front-end Developer',
+    description: 'Overhauled the UX using Material UI, the ReactJS framework in TypeScript, and Redux Toolkit state management, to decrease drop-off rate.\n\nImplemented a guided, in-browser video scanning feature, decreasing user drop-off rate and increasing video input accuracy.\n\nImplemented a front-end behaviour tracking system in JS to decrease operating costs, optimize product team knowledge of application usage, and diagnose user journey choke points.',
+  },
+  {
+    company: 'Lady Ballers Camp',
+    role: 'Software Engineer, Front-end Team Lead, and Product Manager',
+    description: 'Created a mobile Expo React Native application centered around Content Delivery, and Twitter-esque social media interactions.\n\nUsed Sanity Studio to create a custom Content Management System (CMS) and Creation Platform, for platform independent content delivery. Deployed and wrote queries for both the GraphQL and GROQ API endpoints of Sanity Studio.\n\nBuilt a proprietary React Native component library for translating content data delivered from Sanity’s API into natively rendered markdown.\n\nMentored three junior engineers in React fundamentals, the NativeBase and Material UI component libraries, and Redux state management.',
+  },
+  {
+    company: 'Scotiabank\'s Digital Factory',
+    role: 'Software Developer',
+    description: 'I was the only software developer intern brought into the Digital Factory during the summer of 2020.\n\nDeveloped new customer target filtering features to streamline the creation of targeted product campaigns and offers, using ReactJS and Redux.\n\nWon the bank wide Intern Capstone project, with an educational solution to increase customer acquisition across the bank, through a Figma prototype, which prioritized ROI and available tech assets.',
+  },
+  {
+    company: 'catchUp',
+    role: 'Software Developer',
+    description: 'Lead developer and designer in a start-up project, catchUp, a note sharing service that focuses on working with accessibility needs guidelines. Designed the application and flows using Figma. Developing the Front-end using technologies such as ReactJS, React Routing, to deliver a clean minimalist user experience.',
+  },
+  {
+    company: 'NexJ Health Inc.',
+    role: 'Software Developer',
+    description: 'Developed a web and native Android and iOS applications for NexJ Health Inc’s patient-facing healthcare platform as a full stack developer.\n\nBuilt new user functionality and features, while working on the application’s front-end UI through a custom in-house framework in JavaScript, creating back-end services with Node JS, and interacting with the MongoDB database under a Mongoose schema. Worked on the deployment of the web application into native applications through Adobe PhoneGap.',
+  },
+];
+
+const projects = [
+  {
+    name: 'Scotiabank MVP Web Application',
+    collab: 'Technology Leaderhip Initiatve x Scotiabank Collaboration',
+    description: 'Developed a reactive prototype for Scotiabank\'s Global Payments division, mocking Interac’s Request to Pay feature for businesses, to reduce fees/overhead costs for immediate ROI.\n\nDeveloped a customer and internal facing application with ReactJS, for which I created REST APIs for back-end services with ExpressJS in TypeScript.',
+  },
+  {
+    name: 'This Website!',
+    collab: 'Stand alone project',
+    description: 'Created this website from the ground up, using technologies such as React, React Router, all of the latest React Hooks, entirely custom CSS, and Firebase for the back-end.',
+  },
+];
+
+export { experience, projects };
