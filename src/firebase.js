b278d7f1cae3a6f1aa5e2c9689fa7063be52af1c
PatrickVuscan <patrick.vuscan@gmail.com>
2021-06-28 20:57:53 -0400

    Minor refactoring and changes

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDoEvFwbDCM5eZlL6BUYIcgYHELH9d1h4I',
-  authDomain: 'personal-website-vuscan.firebaseapp.com',
-  databaseURL: 'https://personal-website-vuscan.firebaseio.com',
-  projectId: 'personal-website-vuscan',
-  storageBucket: 'personal-website-vuscan.appspot.com',
-  messagingSenderId: '873198924041',
-  appId: '1:873198924041:web:c773bac2c0ed2cac9363d8',
-  measurementId: 'G-JS9CNZVKRT',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
