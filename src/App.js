602fdbd089080af3fa0a54e3cc16b0725af64a48
Patrick Vuscan <pvuscan@riotgames.com>
2023-08-19 08:41:59 -0700

    Remove ReportBug

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ import './App.scss';
 import About from './Components/About/About';
 import Contact from './Components/Contact/Contact';
 import Experience from './Components/Experience/Experience';
-import ReportBug from './Components/ReportBug/ReportBug';
 import ScrollToTop from './Components/General/ScrollToTop';
 
 const App = () => {
@@ -25,7 +24,6 @@ const App = () => {
           <Route exact path={['/', '/about']} render={() => <About />} />
           <Route exact path="/experience" render={() => <Experience />} />
           <Route exact path="/contact" render={() => <Contact />} />
-          <Route exact path="/reportBug" render={() => <ReportBug />} />
         </Switch>
       </ScrollToTop>
       <Footer />
