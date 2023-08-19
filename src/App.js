400d50c3913610f976066af47c63c0c2a4568f71
Patrick Vuscan <pvuscan@riotgames.com>
2023-08-19 08:46:16 -0700

    Init Updated Firebase Setup in App, and load Data

--- src/App.js
+++ src/App.js
@@ -5,15 +5,51 @@ import {
   Route,
   useHistory,
 } from 'react-router-dom';
+
+// Initialize Firebase
+import "./Firebase/SetupFirebase";
+
+// React Components
 import Header from './Components/General/Header';
 import Footer from './Components/General/Footer';
-import './App.scss';
 import About from './Components/About/About';
 import Contact from './Components/Contact/Contact';
 import Experience from './Components/Experience/Experience';
 import ScrollToTop from './Components/General/ScrollToTop';
 
-const App = () => {
+import './App.scss';
+
+// Import backup data
+import localData from "./data.json";
+
+export const DataContext = createContext();
+
+function App() {
+  const [data, setData] = useState({
+    ...localData,
+    loaded: false
+  });
+
+  useEffect(() => {
+    fetch("https://raw.githubusercontent.com/PatrickVuscan/Personal-Website/master/data.json")
+      .then(response => response.json())
+      .then(fetchedData => {
+        console.log(fetchedData);
+        setData({
+          ...fetchedData,
+          loaded: true,
+        });
+      })
+      .catch(error => {
+        console.log(error);
+        console.log("Potentially stale data being used instead.", localData);
+        setData(data => ({
+          ...data,
+          loaded: true,
+        }));
+      });
+  }, []);
+
   const history = useHistory();
 
   return (
