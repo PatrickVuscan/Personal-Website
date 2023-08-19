d3caf9472822682b7d6d9fed19be99b86df42fd4
Patrick Vuscan <pvuscan@riotgames.com>
2023-08-19 08:49:47 -0700

    Major Changes and Refactors to Contact Page

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,12 +1,17 @@
-import React, { useState } from 'react';
-import '../../App.scss';
-import firebase from '../../firebase';
-import { Body, Title, ErrorMessage } from '../General/General';
+import { useState } from "react";
+
+import CallToAction from "./CallToAction";
+import sendEmail from "../../Firebase/SendEmail";
+import { Body, ErrorMessage } from "../General/General";
+import LinkedIn from "./LinkedIn";
+
+import "../../App.scss";
+import "./Contact.scss";
 
 const Contact = () => {
-  const [email, setEmail] = useState('');
-  const [phoneNumber, setPhoneNumber] = useState('');
-  const [message, setMessage] = useState('');
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [message, setMessage] = useState("");
   const [submitted, setSubmitted] = useState(false);
 
   const [emailError, setEmailError] = useState(false);
@@ -14,40 +19,51 @@ const Contact = () => {
   const [messageError, setMessageError] = useState(false);
 
   const validateEmail = () => {
-    const valid = email.length > 0 && /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}/.test(email);
+    const valid = email.length > 0 && /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email);
     setEmailError(!valid);
     return valid;
   };
 
   const validatePhoneNumber = () => {
     const valid = phoneNumber.length === 0 ||
-      /^\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})$/.test(phoneNumber);
+      /^(\+?\d{1,3})?[-. ]?\(?\d{3}\)?[-. ]?\d{3}[-. ]?\d{4}$/.test(phoneNumber);
     setPhoneNumberError(!valid);
     return valid;
   };
 
   const validateMessage = () => {
-    const valid = message.length > 0;
-    // /^[\sa-zA-Z @.:;,?#$%^&*_+=~`"'|(){}\-/\\[\]]+$/.test(message);
+    const valid = message.length > 0 && /^[a-zA-Z0-9.,!?'"\n\r(){}[\]@#$%^&*-_+=:;/<>|~` ]*$/.test(message);
     setMessageError(!valid);
     return valid;
   };
 
-  const submitToDB = () => {
-    firebase.database().ref('contacts').push({
+  const handleSendEmail = () => {
+    sendEmail({
       email,
-      phoneNumber: (phoneNumber) || null,
+      phoneNumber,
       message,
-    });
-  };
+    })
+      .then((result) => {
+        const data = result.data;
+        const message = data.message;
+        console.log(result);
+        console.log(data);
+        console.log(message);
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+  }
 
   const handleSubmitAccountInfo = (event) => {
     event.preventDefault();
+
     if (validateEmail() && validatePhoneNumber() && validateMessage()) {
       setSubmitted(true);
-      submitToDB();
+      handleSendEmail();
       return true;
     }
+
     validateEmail();
     validatePhoneNumber();
     validateMessage();
@@ -55,81 +71,58 @@ const Contact = () => {
   };
 
   return (
-    <>
-      <div className="container-for-centering">
-        <div className="contact">
-          <div style={{ textAlign: 'center' }}>
-            <a href="https://www.linkedin.com/in/patrickvuscan/">
-              <img
-                alt="LinkedIn Logo"
-                src={`${process.env.PUBLIC_URL}/linkedIn.png`}
-                style={{
-                  width: '3rem',
-                  margin: 'auto',
-                }}
-              />
-            </a>
-            <Body style={{
-              textAlign: 'center',
-              lineHeight: '1.5em',
-            }}
-            >
-              Connect with me on LinkedIn!
-            </Body>
-          </div>
-          <Title>Get in touch!</Title>
-          <Body>
-            {'I\'m always looking for new opportunities and exciting projects to take part in! ' +
-            'Whether you have questions, or just want to say hi, send me a message and I\'ll get ' +
-            'back to you soon!'}
-          </Body>
-          <form onSubmit={handleSubmitAccountInfo}>
-            <label className="text-subtitle" htmlFor="email">
-              <span>Email</span>
-              <input
-                id="email"
-                type="text"
-                onBlur={(e) => {
-                  setEmail(e.target.value);
-                }}
-                onKeyDown={(e) => { return e.key !== 'Enter'; }}
-              />
-            </label>
-            { emailError && <ErrorMessage>Please enter a valid email!</ErrorMessage>}
-            <label className="text-subtitle" htmlFor="phoneNumber">
-              <span>Phone Number (Optional)</span>
-              <input
-                id="phoneNumber"
-                type="text"
-                onBlur={(e) => {
-                  setPhoneNumber(e.target.value);
-                }}
-                onKeyDown={(e) => { return e.key !== 'Enter'; }}
-              />
-            </label>
-            { phoneNumberError && <ErrorMessage>Please enter a valid phone number!</ErrorMessage>}
-            <label className="text-subtitle" htmlFor="message">
-              <span>Message</span>
-              <textarea
-                id="message"
-                onBlur={(e) => {
-                  setMessage(e.target.value);
-                }}
-              />
-            </label>
-            { messageError && <ErrorMessage>Please enter a message!</ErrorMessage>}
-            <button type="submit" className="centered">Send</button>
-          </form>
-          {
-            submitted && (
-            <Body>
-              {'Sent! I\'ll get back to you soon :)'}
-            </Body>
-            )
-          }
-        </div>
+    <div className="center-content">
+      <div className="contact">
+        <LinkedIn />
+        <CallToAction />
+
+        <form onSubmit={handleSubmitAccountInfo}>
+          <label htmlFor="email">
+            <span>Email</span>
+            <input
+              id="email"
+              type="text"
+              onBlur={(e) => {
+                setEmail(e.target.value);
+              }}
+              onKeyDown={(e) => { return e.key !== "Enter"; }}
+            />
+          </label>
+
+          {emailError && <ErrorMessage>Please enter a valid email!</ErrorMessage>}
+
+          <label htmlFor="phoneNumber">
+            <span>Phone Number (Optional)</span>
+            <input
+              id="phoneNumber"
+              type="text"
+              onBlur={(e) => {
+                setPhoneNumber(e.target.value);
+              }}
+              onKeyDown={(e) => { return e.key !== "Enter"; }}
+            />
+          </label>
+
+          {phoneNumberError && <ErrorMessage>Please enter a valid phone number!</ErrorMessage>}
+
+          <label htmlFor="message">
+            <span>Message</span>
+            <textarea
+              id="message"
+              onBlur={(e) => {
+                setMessage(e.target.value);
+              }}
+            />
+          </label>
+
+          {messageError && <ErrorMessage>Please enter a message!</ErrorMessage>}
+
+          <button className="text-button centered" type="submit">Send</button>
+        </form>
+
+        {submitted && <Body>{"Sent! I'll get back to you soon :)"}</Body>}
       </div>
-    </>
+    </div>
   );
 };
 
