1600a7e8044a99e1c808cc33003aaa22b8b9e33b
Patrick Vuscan <pvuscan@riotgames.com>
2023-08-19 16:16:11 -0700

    Updated Projects component

--- src/Components/Experience/Projects.jsx
+++ src/Components/Experience/Projects.jsx
@@ -1,47 +1,62 @@
 import { useContext } from "react";
-import { Body, Title, SuperText } from "../General/General";
+import { useMediaQuery } from "react-responsive";
+import {
+  SuperText,
+  Title,
+  Subtitle,
+  Body,
+} from "../General/General";
 import DataContext from "../../DataContext";
 
+// Due to the custom links, couldn't iterate over data.projects
+// TODO: implement solution for handling links specified within data.json
 const Projects = () => {
-  const { projects } = useContext(DataContext);
+  const { projects: { scotiabank, personalWebsite } } = useContext(DataContext);
+  const isMobile = useMediaQuery({ query: "(max-width: 600px)" });
 
-  return (
+  const atLeastOneProject = scotiabank || personalWebsite;
+  const hasBoth = scotiabank && personalWebsite;
+  const shouldDisplayDivider = hasBoth && isMobile;
+
+  return atLeastOneProject ? (
     <div className="green-background">
-      <div className="center-content" style={{ backgroundColor: "#2d8000" }}>
-        <div className="project-experience">
-          <Title>Projects</Title>
-          <div>
-            <SuperText>{projects[0].name}</SuperText>
-            <SuperText className="text-super-sub">
-              {projects[0].collab}
-            </SuperText>
-          </div>
-          <Body>
-            {projects[0].description}
-            {"\n\nYou can demo the project "}
-            <a href="https://request-to-pay.herokuapp.com/">here!</a>
-            {"\nYou can also see the "}
-            <a href="https://github.com/PatrickVuscan/RequestToPay_FrontEnd">front-end repo here</a>
-            {", and the "}
-            <a href="https://github.com/PatrickVuscan/RequestToPay_BackEnd">back-end repo here</a>
-            {" as well!"}
-          </Body>
-          <div>
-            <SuperText>{projects[1].name}</SuperText>
-            <SuperText className="text-super-sub">
-              {projects[1].collab}
-            </SuperText>
-          </div>
-          <Body>
-            {projects[1].description}
-            {"\n\nYou can also see the "}
-            <a href="https://github.com/PatrickVuscan/personal-website">repo here</a>
-            {" too!"}
-          </Body>
+      <div className="center-content five-margin">
+        <div className="information contrasting-colors">
+          <SuperText>Projects</SuperText>
+
+          {scotiabank && (
+            <div>
+              <Title>{scotiabank.name}</Title>
+              <Subtitle>{scotiabank.collab}</Subtitle>
+              <Body>
+                {scotiabank.description}
+                {"\n\nYou can also see the "}
+                <a href="https://github.com/PatrickVuscan/RequestToPay_FrontEnd">front-end repo here</a>
+                {", and the "}
+                <a href="https://github.com/PatrickVuscan/RequestToPay_BackEnd">back-end repo here</a>
+                {" as well!"}
+              </Body>
+            </div>
+          )}
+
+          {shouldDisplayDivider && <hr />}
+
+          {personalWebsite && (
+            <div>
+              <Title>{personalWebsite.name}</Title>
+              <Subtitle>{personalWebsite.collab}</Subtitle>
+              <Body>
+                {personalWebsite.description}
+                {"\n\nYou can also see the "}
+                <a href="https://github.com/PatrickVuscan/personal-website">repo here</a>
+                {" too!"}
+              </Body>
+            </div>
+          )}
         </div>
       </div>
     </div>
-  );
+  ) : <div />;
 };
 
 export default Projects;
